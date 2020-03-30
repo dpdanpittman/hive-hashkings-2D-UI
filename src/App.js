69da077494bad95418317938520c850151061b2a
Daniel Pittman <dpdanpittman@gmail.com>
2020-03-29 22:19:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
       overlayMenuActive: true,
       mobileMenuActive: false,
       localState: {
-        username: "vcdragon",
+        username: "",
         login: username =>
           this.setState(state => ({
             localState: {
