b095d7dc8319b32f7ac80457936f85a4d36e3a81
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-17 09:03:45 -0800

    added dependency useEffect

--- src/components/BuySeed.js
+++ src/components/BuySeed.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import {Button} from "primereact/button";
 import {Dropdown} from "primereact/dropdown";
 import {seedNames, seedTypes} from "../service/HashkingsAPI";
