122aa7e2afe571c4995fd953d425c1cbf6edfade
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-18 14:21:23 -0800

    added sll redirect

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+
 const express = require("express");
 const path = require("path");
 const app = express();
+
+//redirect to ssl
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 
 app.use(express.static(path.join(__dirname, "build")));
 
