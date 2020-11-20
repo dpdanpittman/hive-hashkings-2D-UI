4d0f46eb5659e11eba1dba57d22c6ed0d605cf24
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-19 17:34:17 -0800

    Update SteemConnectAPI.js

--- src/service/SteemConnectAPI.js
+++ src/service/SteemConnectAPI.js
@@ -1,10 +1,10 @@
 import hivesigner from "hivesigner";
 
 const api = new hivesigner.Client({
-  app: "hive-loginking",
+  app: "loginking",
   baseURL: "https://hivesigner.com",
   callbackURL:
-    process.env.REACT_APP_SC_CALLBACK || "https://hive.hashkings.app/callback",
+    process.env.REACT_APP_SC_CALLBACK || "https://www.hashkings.app/callback",
   scope: ["custom_json", "login"]
 });
 
