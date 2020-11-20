a8bbc30f3a627daa0bdf2d278534ad29feeb16a7
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-19 17:45:49 -0800

    Update InsideCard.js

--- src/components/InsideCard.js
+++ src/components/InsideCard.js
@@ -15,8 +15,6 @@ export default function MediaCard() {
   const classes = useStyles();
 
   return (
-    <Box boxShadow={0}>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/ju9c5_wqUcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </Box>
   );
 }
