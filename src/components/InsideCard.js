f8d771cb7469de0a64c7b9bb179c0210863d2763
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-19 18:02:32 -0800

    added paper

--- src/components/InsideCard.js
+++ src/components/InsideCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+
 
 const useStyles = makeStyles({
   card: {
@@ -15,6 +16,8 @@ export default function MediaCard() {
   const classes = useStyles();
 
   return (
+    <Paper>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/ju9c5_wqUcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Paper>
   );
 }
