12329ec06ac04fff0a5fe596804d2758d4282ae4
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-21 19:59:43 -0800

    Update Trading.js

--- src/components/GardenPage/Trading.js
+++ src/components/GardenPage/Trading.js
@@ -126,7 +126,7 @@ export const Trading = () => {
         setLoading(true);
         hashkingsApi.getDGPO().then(dgpo => {
           const spv =
-            parseFloat(dgpo.total_vesting_fund_steem.split(" ")[0]) /
+            parseFloat(dgpo.total_vesting_fund_hive.split(" ")[0]) /
             parseFloat(dgpo.total_vesting_shares.split(" ")[0]);
           Promise.all([
             hashkingsApi
