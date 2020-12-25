daefed2fe29fa02d9b7aa8db86776d46027f816d
Daniel Pittman <dpdanpittman@gmail.com>
2020-12-25 00:06:35 -0800

    Update TutorialCard.js

--- src/components/TutorialCard.js
+++ src/components/TutorialCard.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import { FarmIcon } from './Icons';
 import { GerminateIconBlack } from './Icons';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
   card: {
