3b9f0d07af664ecd6434af8500b07fb52b0c941e
Daniel Pittman <dpdanpittman@gmail.com>
2021-01-21 13:51:58 -0800

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import MediaCard from './FarmingCard';
@@ -72,6 +72,356 @@ export const HomePage = () => {
 const classes = useStyles();
 const isDesktop = window.innerWidth < 1000;
 const image1 = "https://i.imgur.com/j2CGYh2.jpg";
+const [seedAsia, setSeedAsia] = useState();
+const [seedAfrica, setSeedAfrica] = useState();
+const [seedAfghanistan, setSeedAfghanistan] = useState();
+const [seedJamaica, setSeedJamaica] = useState();
+const [seedMexico, setSeedMexico] = useState();
+const [seedSouthAmerica, setSeedSouthAmerica] = useState();
+const [seedAsiaPrices, setAsiaPrices] = useState([0]);
+const [seedAfricaPrices, setAfricaPrices] = useState([0]);
+const [seedAfghanistanPrices, setAfghanistanPrices] = useState([0]);
+const [seedJamaicaPrices, setJamaicaPrices] = useState([0]);
+const [seedMexicoPrices, setMexicoPrices] = useState([0]);
+const [seedSouthAmericaPrices, setSouthAmericaPrices] = useState([0]);
+
+const loadPriceData = async () => {
+    
+  const urlAPI = 'https://hashkings-api.herokuapp.com/';
+  
+  const response = await fetch(urlAPI);
+  const pricedata = await response.json();
+
+  var seedAsiaPrice = pricedata.stats.prices.asiaBundle.price;
+  var seedAfricaPrice = pricedata.stats.prices.africaBundle.price;
+  var seedAfghanistanPrice = pricedata.stats.prices.AfghanistanBundle.price;
+  var seedJamaicaPrice = pricedata.stats.prices.jamaicaBundle.price;
+  var seedMexicoPrice = pricedata.stats.prices.mexicoBundle.price;
+  var seedSouthAmericaPrice = pricedata.stats.prices.southAmericaBundle.price;
+
+  setAsiaPrices(seedAsiaPrice);
+  setAfricaPrices(seedAfricaPrice);
+  setAfghanistanPrices(seedAfghanistanPrice);
+  setJamaicaPrices(seedJamaicaPrice);
+  setMexicoPrices(seedMexicoPrice);
+  setSouthAmericaPrices(seedSouthAmericaPrice);
+}
+
+useEffect(() => {
+  loadPriceData();
+}, []);
+
+const handleSubmitAsia = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `asia_bundle`;
+    const to = "hashkings";
+    const amount = seedAsiaPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedAsia();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
+
+const handleSubmitAfrica = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `africa_bundle`;
+    const to = "hashkings";
+    const amount = seedAfricaPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedAfrica();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
+
+const handleSubmitAfghanistan = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `afghanistan_bundle`;
+    const to = "hashkings";
+    const amount = seedAfghanistanPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedAfghanistan();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
+
+const handleSubmitJamaica = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `jamaica_bundle`;
+    const to = "hashkings";
+    const amount = seedJamaicaPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedJamaica();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
+
+const handleSubmitMexico = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `mexico_bundle`;
+    const to = "hashkings";
+    const amount = seedMexicoPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedMexico();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
+
+const handleSubmitSouthAmerica = async e => {
+  e.preventDefault();
+  if (username) {
+    setIsSubmitting(true);
+
+    const memo = `southAmerica_bundle`;
+    const to = "hashkings";
+    const amount = seedSouthAmericaPrices.toFixed(3).toString();
+    const currency = "HIVE";
+
+    if (hasHiveKeychain()) {
+      const hive_keychain = window.hive_keychain;
+      try {
+        await new Promise((resolve, reject) => {
+          return hive_keychain.requestTransfer(
+            username,
+            to,
+            amount,
+            memo,
+            currency,
+            response => {
+              if (response.success) {
+                resolve(response);
+              } else {
+                reject();
+              }
+            },
+            true
+          );
+        });
+        setIsSubmitting(false);
+        setSeedSouthAmerica();
+      } catch {
+        setIsSubmitting(false);
+      }
+    } else {
+      window.location.href = sign(
+        "transfer",
+        {
+          to,
+          from: username,
+          amount: `${amount} ${currency}`,
+          memo
+        },
+        process.env.REACT_APP_URL
+          ? `${process.env.REACT_APP_URL}/`
+          : "https://localhost:3000/"
+      );
+    }
+  }
+};
 
 if (!isDesktop) {
   return(
@@ -102,29 +452,7 @@ if (!isDesktop) {
       <Grid item xs={3}>
       </Grid>
     </Grid>
-        <Grid item xs={3}>
-     
-        </Grid>
-        <Grid item xs={6}>
-          <InsideCard />
-        </Grid>
-        <Grid item xs={3}>
-         
-        </Grid>
-      </Grid>
-      <br/>
-      <hr/>
-      <br/>
-
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-        <center>
-    <img src="https://i.imgur.com/Yzzk8H0.png"></img>
-    </center>
-        </Grid>
-        <Grid item xs={12}>
-         <TrendingHomePage /> 
-        </Grid>
+        
       </Grid>
       <br/>
       {/*<hr/>
