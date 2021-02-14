c6325f3c5ab0830bb0a8f06bd85ae09f7bf548ad
Daniel Pittman <dpdanpittman@gmail.com>
2021-02-14 09:13:58 -0800

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -106,7 +106,7 @@ const hasHiveKeychain = useHiveKeychain();
 
 const loadPriceData = async () => {
     
-  const urlAPI = 'https://hashkings.xyz:3000';
+  const urlAPI = 'https://hashkings.xyz/';
   
   const response = await fetch(urlAPI);
   const pricedata = await response.json();
